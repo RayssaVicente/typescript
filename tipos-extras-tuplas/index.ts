bfade21f67859c342f01ab83818f5ba7ca84e7f4
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-03-13 14:05:32 -0300

    Tipos extras tuplas

---
+++ tipos-extras-tuplas/index.ts
@@ -0,0 +1,13 @@
+let points: [number, number]
+
+points = [1,3]
+
+let cruew: [string, string]
+
+cruew[0] = 'Selena'
+cruew[1] = 'Gomes'
+cruew[2] = 'julia'
+
+let point: [number, number, boolean, string]
+
+point = [2,5, true, 'safira']
