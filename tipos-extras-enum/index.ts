d31994a32844e0df4ffd46a3b2618d9150346707
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-03-13 14:05:19 -0300

    Tipos extras enum

---
+++ tipos-extras-enum/index.ts
@@ -0,0 +1,13 @@
+enum planets {
+    MERCURIO = 1,
+    MARTE,
+    VENUS,
+    TERRA
+}
+
+enum PERSON {
+    LUIZA = 'Luiza',
+    JULIA = 'Juliana',
+    MARIA = 'Maria',
+    DANIELA = 'Daniela'
+}
