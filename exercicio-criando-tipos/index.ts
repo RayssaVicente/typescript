b9b9fa53c47660d3fb6ca5e928fbdff479b496ba
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-03-26 09:05:54 -0300

    continuação da resolução do exercício

--- exercicio-criando-tipos/index.ts
+++ exercicio-criando-tipos/index.ts
@@ -50,3 +50,66 @@ function removeSatellite(name: string, planet: Planet){
     alert(`O satelite ${name} foi removido do planeeta ${planet.name}.`)
 }
 
+function promptValidSituation(){
+    let situation: PlanetSituation
+    let validSituation = false
+
+    while (!validSituation){
+        const situationInput = prompt("Informe a situação do planeta:\n1 - Habitado\n2 - Habitado\n3 - Inabitado\n4- Inexplorado")
+
+        switch(situationInput){
+            case "1":
+                situation = "Habitado"
+                validSituation = true
+                break
+            case "2":
+                situation = "Habitavel"
+                validSituation = true
+                break
+            case "3":
+                situation = "Inabitavel"
+                validSituation = true
+                break
+            case "4":
+                situation = "Inexplorado"
+                validSituation = true
+                break
+            default:
+                alert("Situação inválida!")
+                break;
+        }
+    }
+
+    return situation
+}
+
+function promptValidPlanet(callback: (planet: Planet) => void){
+    const planetName = prompt('Informe o nome do planeta: ')
+    const planet = findPlanet(planetName)
+
+    if(planet){
+        callback(planet)
+    } else{
+        alert('Planeta não encontrado! Retornando ao menu...')
+    }
+
+}
+
+function firstMenuOption(){
+    const name = prompt('Informe o nome do planeta: ')
+    const coordinateA = Number(prompt('Informe a primeira coordenada: '))
+    const coordinateB = Number(prompt('Informe a segunda coordenada: '))
+    const coordinateC = Number(prompt('Informe a terceira coordenada: '))
+    const coordinateD = Number(prompt('Informe a quarta coordenada: '))
+
+    const situation = promptValidSituation()
+
+    const confirmation = confirm(`Confirma o registro do planeta ${name}? Coordenadas: (${coordinateA}, ${coordinateB}, ${coordinateC}, ${coordinateD}),
+    Situação: ${situation}`)
+
+    if(confirmation){
+        addPlanet(name, [coordinateA, coordinateB, coordinateC, coordinateD], situation)
+    }
+}
+
+
